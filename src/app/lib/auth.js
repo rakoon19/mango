3d3135842df9837f5aaf3468f51bb7bef1ff2cf5
rakoon19 <rahatakondo18@gmail.com>
2026-04-30 09:27:07 +0600

    db name

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -4,9 +4,10 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { username } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGO_URI);
-const db = client.db();
+const db = client.db("user");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
