4d635b7d07dc2270bb64517ac01bc8ad73ef4039
rakoon19 <rahatakondo18@gmail.com>
2026-04-30 09:05:04 +0600

    production error

--- src/app/books/[id]/page.jsx
+++ src/app/books/[id]/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 import { useParams, useRouter } from 'next/navigation';
 import Image from 'next/image';
-import Link from 'next/link';
 import { useEffect } from 'react';
 import { authClient } from '@/app/lib/auth-client';
 import BOOKS_DATA from '../../../data/BOOKS_DATA.json';
