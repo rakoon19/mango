01ffb2b2f3c497a9d0a03c37b574132ef987282c
rakoon19 <rahatakondo18@gmail.com>
2026-05-17 20:03:36 +0600

    fix prod

--- src/app/lib/auth-client.js
+++ src/app/lib/auth-client.js
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
     baseURL: process.env.NODE_ENV === "production" 
-        ? "https://mango-app.vercel.app" 
+        ? "https://mango-rosy.vercel.app"  
         : "http://localhost:3000"
 })
